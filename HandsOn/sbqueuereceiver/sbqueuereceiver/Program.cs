47e557411537bdafe539c449a920f92a58b4ff4a
sinharajeshk <sinha.rajeshk@hotmail.com>
2017-06-30 00:47:35 +0530

    Hands on exercises on azure service bus.

---
+++ HandsOn/sbqueuereceiver/sbqueuereceiver/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sbqueuereceiver
+{
+    class Program
+    {
+        static string ConnectionString = "Endpoint=sb://rajeshservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=jho4DKo96odkKFuz+vxp67nQuFlBR/ctn9XHqALaqnE=";
+        static string QueuePath = "sbqueue";
+
+
+        static void Main(string[] args)
+        {
+            //Service Bus Queue Receiver
+            var queueClient = QueueClient.CreateFromConnectionString(ConnectionString, QueuePath);
+
+            queueClient.OnMessage(msg => ProcessMessage(msg));
+
+            Console.WriteLine("Press Enter to Exit...");
+            Console.ReadLine();
+
+            queueClient.Close();
+        }
+
+        private static void ProcessMessage(BrokeredMessage msg)
+        {
+            var text = msg.GetBody<string>();
+            Console.WriteLine("\nReceived Messages : " + text);
+        }
+
+
+    }
+}
---
+++ HandsOn/sbqueuesender/sbqueuesender/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sbqueuesender
+{
+    class Program
+    {
+        static string ConnectionString = "Endpoint=sb://rajeshservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=jho4DKo96odkKFuz+vxp67nQuFlBR/ctn9XHqALaqnE=";
+        static string QueuePath = "sbqueue";
+
+        static void Main(string[] args)
+        {
+            //Service Bus Queue Sender
+            var queueClient = QueueClient.CreateFromConnectionString(ConnectionString, QueuePath);
+            for (int i = 0; i < 10; i++)
+            {
+                var message = new BrokeredMessage("Sender's Message ==> " + i);
+
+
+                //      message.SessionId = “test”;
+
+
+                queueClient.Send(message);
+                Console.Write("\nSent Message : = " + i);
+            }
+
+            Console.WriteLine("Press Enter to Exit...");
+            Console.ReadLine();
+            queueClient.Close();
+        }
+    }
+}
